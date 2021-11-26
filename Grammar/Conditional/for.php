d71b0e4eb0bee3dc0e67ea0ac77760ab03d8ac07
gpflawkd2 <gpflawkd2@gmail.com>
2021-11-26 14:32:46 +0900

    Organize folders

--- Grammar/Conditional/for.php
+++ Grammar/Conditional/for.php
@@ -71,4 +71,24 @@ for($i = 0; $i < 10; $i++){
 
 echo $sum;
 
+//ex
+
+for($j = 0; $j < 10; $j++){
+
+    if($j === 5){
+        //break;    //종료
+        continue;   //해당 조건만 제외하고 반복문 계속 실행
+    }
+
+    //echo "coding everybody".$j."<br />";
+    echo "coding everybody{$j}<br />";
+    //echo "coding everybody".($j*2)."<br />";
+}
+
+for($i = 0; $i < 10; $i++){    
+    for($j = 0; $j < 10; $j++){  
+        echo "coding everybody{$i}{$j}<br />";
+    }
+}
+
 ?>
